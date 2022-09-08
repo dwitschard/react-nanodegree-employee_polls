12d0b6e0b6c57705b38b05654d0ce87ab38409d2
Dominik Witschard <dominik.witschard@zuehlke.com>
2022-09-08 16:18:48 +0200

    feat: improved 404 page

--- src/features/404/NotFoundPage.js
+++ src/features/404/NotFoundPage.js
@@ -1,5 +1,12 @@
 const NotFoundPage = () => {
-  return <div>Page Not Found!</div>;
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="text-9xl uppercase text-primary">404</div>
+        <div className="pl-5 text-3xl text-secondary">Page not Found</div>
+      </div>
+    </>
+  );
 };
 
 export default NotFoundPage;
