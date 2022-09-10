0bfda938249089df60e737acfde7d45c3902e63a
Dominik Witschard <dominik.witschard@zuehlke.com>
2022-09-10 10:10:34 +0200

    feat: visual indicator for disabled button

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,9 +10,10 @@ const Button = ({
       disabled={disabled}
       onClick={handleClick}
       type={buttonType}
-      className={`px-3 py-1 text-xl uppercase rounded bg-primary text-tertiary ${className} ${
-        !disabled && "hover:cursor-pointer hover:bg-accentPrimary"
-      } `}
+      className={`px-3 py-1 text-xl uppercase rounded bg-primary text-tertiary ${className} 
+        ${!disabled && "hover:cursor-pointer hover:bg-accentPrimary"}
+        ${disabled && "bg-secondary/50 text-white"}  
+      `}
     >
       <pre>{disabled}</pre>
       {children}
